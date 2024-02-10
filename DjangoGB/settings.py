ea2c330550bc63442a00f82bb3d30a45bbe8b96a
TotskayaOV <olga.totskaya@gmail.com>
2024-02-10 13:23:36 +0700

    created homework 1

--- DjangoGB/settings.py
+++ DjangoGB/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'DjangoGB.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'myapp/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,3 +123,37 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+         'console': {
+            'class': 'logging.StreamHandler',
+             'formatter': 'verbose'
+            },
+         'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'django.log',
+            'formatter': 'verbose',
+             'encoding': 'UTF-8'
+            },
+         },
+     'loggers': {
+         'django': {
+             'handlers': ['console', 'file'],
+             'level': 'INFO',
+         },
+     'myapp': {
+         'handlers': ['console', 'file'],
+         'level': 'DEBUG',
+         'propagate': True,
+        },
+    },
+}
